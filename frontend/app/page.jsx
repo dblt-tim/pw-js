a57eb96be6e2bd9b35ee5d17637ebfc166a7b201
dblt-tim <timeodiebolt@gmail.com>
2026-03-16 14:24:47 +0100

    init frontend

---
+++ frontend/app/page.jsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { useState } from 'react'
+
+
+export default function Page() {
+
+  const [count, setCount] = useState(0)
+
+  function addOne() {
+    setCount(count + 1)
+  }
+
+  return <>
+    <h1>Hello World !</h1>
+
+    <button style={{width:"50px", height:"50px"}}onClick={addOne}>{ count }</button>
+  </>
+}
