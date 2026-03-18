bbc887b8083f0b0b44ccb37a42b23f2bd9c586f0
dblt-tim <timeodiebolt@gmail.com>
2026-03-18 16:29:40 +0100

    added .env entry to .gitignore just in case

---
+++ app/map/page.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+/* documentation MapBox : https://docs.mapbox.com/mapbox-gl-js/guides/get-started/use-with-npm/
+ * attention à utiliser le code pour React
+ */
+
+function Map() {
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
+
+  useEffect(() => {
+    mapboxgl.accessToken = "--TODO--";
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: [2.349014, 48.864716],
+      zoom: 9,
+    });
+    return () => mapRef.current.remove();
+  }, []);
+  
+  return <section id='map-container' ref={mapContainerRef} />
+}
+
+export default Map
