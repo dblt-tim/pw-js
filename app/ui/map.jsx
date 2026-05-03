4265f377227592e8b8237bea4e708727f38d0e35
kant-0 <quentinfred2006@outlook.fr>
2026-05-03 23:30:53 +0200

    carte

---
+++ app/ui/map.jsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import 'dotenv/config'
+
+mapboxgl.accessToken = process.env.MAPBOXGL_PUBLIC_TOKEN
+
+export default function Map() {
+  const mapRef = useRef(null)
+  const mapContainerRef = useRef(null)
+
+  useEffect(() => {
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: [-71.06776, 42.35816], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+      zoom: 9 // starting zoom
+    });
+
+    return () => {
+        if (Map.current) {
+            mapRef.current.remove()
+        }
+    }
+  }, [])
+
+  return (
+    <>
+      <div id='map-container' ref={mapContainerRef}/>
+    </>
+  )
+}
--- app/voyager/page.jsx
+++ app/voyager/page.jsx
@@ -1,10 +1,12 @@
 "use client"
 
 import '@/app/styles/pages/voyager.css'
+import Map from '@/app/ui/map.jsx'
+
 
 export default function Travel() {
 
     return <main>
-        
+        <Map/>
     </main>
 }
